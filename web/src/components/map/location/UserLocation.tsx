b77ba858b09fdb810acf060e7027b848333d8401
RinseV <43185712+RinseV@users.noreply.github.com>
2021-10-17 16:02:03 +0200

    feat: added user location indicator to location

---
+++ web/src/components/map/location/UserLocation.tsx
@@ -0,0 +1,56 @@
+import { useColorModeValue } from '@chakra-ui/color-mode';
+import { chakra, keyframes } from '@chakra-ui/system';
+import { LngLat } from 'mapbox-gl';
+import React from 'react';
+import { Marker } from 'react-mapbox-gl';
+
+const pulse = keyframes`
+0%   { transform: scale(1); opacity: 1; }
+70%  { transform: scale(3); opacity: 0; }
+100% { transform: scale(1); opacity: 0; }
+`;
+
+type UserLocationProps = {
+    userLocation: LngLat | null;
+};
+
+export const UserLocation: React.VFC<UserLocationProps> = ({ userLocation }) => {
+    const locationColor = useColorModeValue('cyan.600', 'cyan.400');
+    const pulseAnimation = `${pulse} 2s infinite`;
+
+    if (!userLocation) {
+        return null;
+    }
+
+    return (
+        <Marker coordinates={userLocation.toArray()}>
+            <chakra.div position="relative">
+                {/* Indicator with pulse animation */}
+                <chakra.div
+                    w={4}
+                    h={4}
+                    backgroundColor={locationColor}
+                    borderRadius="50%"
+                    _before={{
+                        backgroundColor: locationColor,
+                        content: '""',
+                        width: 4,
+                        height: 4,
+                        borderRadius: '50%',
+                        position: 'absolute',
+                        animation: pulseAnimation
+                    }}
+                    _after={{
+                        borderRadius: '50%',
+                        border: '2px solid #fff',
+                        content: '""',
+                        height: 4,
+                        width: 4,
+                        position: 'absolute',
+                        'box-shadow': '0 0 3px rgba(0, 0, 0, 0.35)'
+                    }}
+                />
+            </chakra.div>
+        </Marker>
+    );
+};

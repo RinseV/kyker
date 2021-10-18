b8e356db3bbc63627df9dcc852063a2e51006f4c
RinseV <43185712+RinseV@users.noreply.github.com>
2021-10-18 19:18:31 +0200

    fix: remove unnecessary serializablecheck

--- web/src/store/store.ts
+++ web/src/store/store.ts
@@ -17,8 +17,7 @@ export const store = configureStore({
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-                ignoredActionPaths: ['preferences.setQueryDate']
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
             }
         })
 });

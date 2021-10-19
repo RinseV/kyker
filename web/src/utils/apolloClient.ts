25ededdc77b12f74aab5ad357f29290295c31ef7
Koen Vlaswinkel <koen@vlaswinkel.info>
2021-10-19 20:42:36 +0200

    feat: switch from CRA to Vite

--- web/src/utils/apolloClient.ts
+++ web/src/utils/apolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: process.env.REACT_APP_BACKEND_URL,
+    uri: import.meta.env.VITE_APP_BACKEND_URL as string,
     cache: new InMemoryCache()
 });

0f8d5c0524a039a57b1acb22458cf85349451990
RinseV <43185712+RinseV@users.noreply.github.com>
2021-10-19 16:44:38 +0200

    fix: test environment variables

--- server/test/setupJest.ts
+++ server/test/setupJest.ts
@@ -1,8 +1,9 @@
 jest.setTimeout(60000);
 process.env.NODE_ENV = 'test';
-process.env.DB_NAME = 'kruger-test';
-process.env.DB_USER = 'postgres';
-process.env.DB_PASSWORD = 'postgres';
+process.env.POSTGRES_HOST = 'localhost';
+process.env.POSTGRES_DB = 'kruger-test';
+process.env.POSTGRES_USER = 'postgres';
+process.env.POSTGRES_PASSWORD = 'postgres';
 process.env.PORT = '4001';
 process.env.REDIS_URL = '127.0.0.1:6379';
 process.env.SESSION_SECRET = 'testing_secret';

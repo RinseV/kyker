95116df75f7ece05570e221aee7d3046e58b1d3a
RinseV <43185712+RinseV@users.noreply.github.com>
2022-01-01 11:08:39 +0100

    Update server/src/migrations/Migration20220101084109.ts
    Co-authored-by: Koen Vlaswinkel <koesie10@users.noreply.github.com>

--- server/src/migrations/Migration20220101084109.ts
+++ server/src/migrations/Migration20220101084109.ts
@@ -3,10 +3,6 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20220101084109 extends Migration {
 
   async up(): Promise<void> {
-    this.addSql('alter table "animal" drop constraint if exists "animal_disabled_check";');
-    this.addSql('alter table "animal" alter column "disabled" type bool using ("disabled"::bool);');
-    this.addSql('alter table "animal" alter column "disabled" set not null;');
-
     this.addSql('alter table "spotting" add column "visibility" int4 null, add column "traffic" int4 null;');
   }
 

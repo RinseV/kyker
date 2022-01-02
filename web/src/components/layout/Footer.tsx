7600daa374f2a4cce2ae5b80819b52887961c76d
RinseV <43185712+RinseV@users.noreply.github.com>
2022-01-02 09:50:13 +0200

    fix: update footer

--- web/src/components/layout/Footer.tsx
+++ web/src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const Footer: React.VFC = () => {
     return (
         <Flex alignItems="center" py={1} px={2}>
-            <Text>2021 - Kyker</Text>
+            <Text>{new Date().getFullYear()} - Kyker</Text>
         </Flex>
     );
 };

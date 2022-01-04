aa95c1f882a196aec7e424b40f23acf8f7a5019b
Koen Vlaswinkel <koen@vlaswinkel.info>
2022-01-04 11:47:29 +0200

    feat: Make map work fully offline including fonts

--- app/src/components/map/Map.tsx
+++ app/src/components/map/Map.tsx
@@ -1,10 +1,12 @@
 import { LogBox } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import { Box, Flex } from 'native-base';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAssets } from 'expo-asset';
 import AppLoading from 'expo-app-loading';
-import mapStyle from '../../../assets/kyker.json';
+import * as FileSystem from 'expo-file-system';
+import { unzip } from 'react-native-zip-archive';
+import mapStyle from '../../../assets/osm_liberty.json';
 
 // Value does not matter
 MapboxGL.setAccessToken('');
@@ -21,19 +23,52 @@ const mapBounds: { ne: [number, number]; sw: [number, number] } = {
     sw: [32.692252, -22.16947]
 };
 
-export const Map: React.VFC = () => {
-    const [assets, error] = useAssets([require('../../../assets/kruger.mbtiles')]);
+const glyphsPath = `${FileSystem.cacheDirectory}glyphs`;
+const spritesPath = `${FileSystem.cacheDirectory}sprites`;
 
-    if (!assets || error) {
+export const Map: React.VFC = () => {
+    const [assets, error] = useAssets([
+        require('../../../assets/kruger.mbtiles'),
+        require('../../../assets/glyphs.zip'),
+        require('../../../assets/sprites.zip')
+    ]);
+
+    const [isLoading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        if (!assets?.[1] || !assets?.[2]) {
+            return;
+        }
+
+        (async () => {
+            // Glyphs
+            const glyphsInfo = await FileSystem.getInfoAsync(glyphsPath);
+            if (!glyphsInfo.exists) {
+                await unzip(assets[1].localUri, FileSystem.cacheDirectory);
+            }
+
+            // Sprites
+            const spritesInfo = await FileSystem.getInfoAsync(spritesPath);
+            if (!spritesInfo.exists) {
+                await unzip(assets[2].localUri, FileSystem.cacheDirectory);
+            }
+
+            setLoading(false);
+        })();
+    }, [assets]);
+
+    if (!assets || error || isLoading) {
         return <AppLoading />;
     }
 
     const styleJSON = JSON.stringify({
         ...mapStyle,
+        glyphs: `${glyphsPath}/{fontstack}/{range}.pbf`,
+        sprite: `${spritesPath}/sprite`,
         sources: {
             ...mapStyle.sources,
-            composite: {
-                ...mapStyle.sources.composite,
+            openmaptiles: {
+                ...mapStyle.sources.openmaptiles,
                 url: `mbtiles://${assets[0].localUri}`
             }
         }
